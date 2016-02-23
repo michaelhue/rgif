9f9adcb56767dfeeef9f6d22868120bc9dd1077c
Michael Hüneburg <hello@michaelhue.com>
2016-02-23 01:36:44 +0100

    First refactor pass
    First pass at breaking up the code into multiple functions. My main motivation are the repeating error checks. Seems that each function should only set and check one err variable at the most. Will explore this further.

--- main.go
+++ main.go
@@ -1,57 +1,56 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
-    "encoding/json"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "github.com/atotto/clipboard"
+	"encoding/json"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
-type Message struct {
-    Url string
+type Response struct {
+	Url string
 }
 
 func help() {
-    fmt.Println("The right gif, every time, in your command line!\nPowered by rightgif.com\n")
-    fmt.Println("Usage: rgif [search]\n")
-    fmt.Println("> rgif oh boy\n> rgif whatever\n> rgif no no no\n")
-    os.Exit(1)
+	fmt.Println("The right gif, every time, in your command line!\nPowered by rightgif.com\n")
+	fmt.Println("Usage: rgif [query]\n")
+	fmt.Println("> rgif oh boy\n> rgif whatever\n> rgif no no no\n")
 }
 
-func search(query string) {
-    var m Message
-    
-    resp, err := http.PostForm("https://rightgif.com/search/web",
-        url.Values{"text": {query}})
+func search(query string) string {
+	var data Response
 
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+	resp, err := http.PostForm("https://rightgif.com/search/web",
+		url.Values{"text": {query}})
 
-    err = json.Unmarshal(body, &m)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
-    clipboard.WriteAll(m.Url)
-    fmt.Println("✓ gif copied to clipboard")
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	return data.Url
 }
 
 func main() {
-    args := os.Args[1:]
-    query := strings.Join(args, " ")
+	args := os.Args[1:]
+	query := strings.Join(args, " ")
 
-    if len(args) == 0 {
-        help()
-    } 
+	if len(args) == 0 {
+		help()
+		return
+	}
 
-    search(query)
+	url := search(query)
+	clipboard.WriteAll(url)
+	fmt.Println("✓ gif copied to clipboard")
 }
